2e39b97c77e45463ea33c162f9a5d9180695aa37
Remano <remanovictor@icloud.coms>
2019-11-04 23:53:05 +0100

    completed assignment

---
+++ scripts/ds-nav.js
@@ -0,0 +1,8 @@
+// ds-ham-nav
+let mobileNav= document.querySelector("#hammy");
+
+mobileNav.addEventListener("click", function() {
+  
+    mobileNav.classList.toggle("is-active");
+    
+});
--- scripts/script.js
+++ scripts/script.js
@@ -17,11 +17,24 @@ hamburger.addEventListener("click", function() {
 
 
 
-// ds-ham-nav
-let mobileNav= document.getElementById("hammy");
 
-mobileNav.addEventListener("click", function() {
-  
-    mobileNav.classList.toggle("is-active");
-    
-});
+//lazy loading
+
+if ('loading' in HTMLImageElement.prototype) {
+  //Native Lazy Loading Supported!
+
+  console.log('I am working');
+
+  const images = document.querySelectorAll('img[loading="lazy"]')
+  images.forEach(img => {
+      img.src = img.dataset.src;
+  })
+} else {
+  // Not supported!
+
+  console.log(' I am not')
+
+  const script = document.createElement('script')
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js';
+  document.body.appendChild(script);
+}
